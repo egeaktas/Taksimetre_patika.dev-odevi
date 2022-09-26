b50cc71360f5d9b77517b547f36e0e00763546eb
Ege Aktas <egeaktas12@gmail.com>
2022-09-26 13:28:43 +0300

    seond

--- src/Pratik4.java
+++ src/Pratik4.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Pratik4 {
+import java.util.Scanner;
+public class Pratik4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double km, km_fiyat = 2.2, acilis = 10, tutar;
+        System.out.println("kaç km gidildi: ");
+        km = input.nextDouble();
+        tutar = acilis + km*km_fiyat;
+        if (tutar<20){
+            tutar = 20;
+        }
+
+        System.out.print(tutar + " tl tuttu.");
+    }
 }
